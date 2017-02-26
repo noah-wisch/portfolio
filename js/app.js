42f45367f425d1b1e7ec0dc4505fcb82fe6d67c4
Noah Wisch <noahwisch@The-Intersect.local>
2017-02-25 19:19:17 -0500

    gulp does not function, router is half-baked

--- js/app.js
+++ js/app.js
@@ -1,2 +1,20 @@
-const app = angular.module('PortfolioApp', []);
+const app = angular.module('PortfolioApp', ['ui.router']);
 
+/* Controllers */
+const controllers = [
+	require('./controllers/resume'),
+	require('./controllers/about'),
+];
+
+for (let i = 0; i < controllers.length; i++) {
+	app.controller(controllers[i].name, controllers[i].func);
+}
+
+/* Routes */
+const routes = require('./routes');
+
+app.config($stateProvider => {
+	for (let i = 0; i < routes.length; i++) {
+		$stateProvider.state(routes[i]);
+	}
+});
